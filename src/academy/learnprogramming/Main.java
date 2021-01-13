5c168e5ae4928b0373921f51c8a6cfb62716f631
Christopher Rumble <christopher.rumble@MacBook-Pro.hsd1.ca.comcast.net>
2021-01-12 21:35:25 -0800

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,35 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // 8 primitive data types
+        // byte
+        // short
+        // int*
+        // long
+        // float
+        // double*
+        // char
+        // boolean*
+        // create your own data types as Classes
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to " + myString);
+        String numberString = numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        String lastString = "10";
+        int myInt = 50;
+        double doubleNumber = 120.47d;
+        lastString = lastString + myInt;
+        System.out.println("Last String is equal to " + lastString);
+        lastString = lastString + doubleNumber;
+        System.out.println("Last String is equal to " + lastString);
+
+    }
+}
